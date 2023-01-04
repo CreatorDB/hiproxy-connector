6294366cb02c98446c312f0d1ec1708a1d972c86
Andy <poi5305@gmail.com>
2023-01-04 15:40:29 +0800

    add lib

---
+++ lib/cjs/hiProxyConnector.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="node" />
+import Request from 'request';
+import { ProxyServer } from './struct';
+type RequestOption = Request.CoreOptions & Request.RequiredUriUrl & {
+    lookup?: (domain: any, options: any, callback: any) => void;
+};
+export declare class HiProxyConnector {
+    masterIP: string;
+    masterPort: number;
+    socksPort: number;
+    constructor(masterIP: string, masterPort?: number, socksPort?: number);
+    getProxyServers(useCountLessThan: number): Promise<ProxyServer[]>;
+    occupyProxy(allowUsingCount: number, retry?: number): Promise<null | ProxyServer>;
+    releaseProxy(proxyId: string, refresh: boolean): Promise<void>;
+    releaseAllProxies(refresh: boolean): Promise<void>;
+    wrapOptionWithProxyServer(option: RequestOption, proxyServer: ProxyServer, ipFamily: 4 | 6): RequestOption;
+    socksRequestWithOptions(options: RequestOption[], refresh: boolean): Promise<{
+        proxyServer: ProxyServer;
+        results: (string | Buffer | Error)[];
+    }>;
+}
+export {};
---
+++ lib/cjs/struct.d.ts
@@ -0,0 +1,12 @@
+export interface RespProxyServer {
+    server: ProxyServer | null;
+}
+export interface RespProxyServers {
+    servers: ProxyServer[];
+}
+export interface ProxyServer {
+    id: string;
+    ipv4: string;
+    ipv6: string;
+    usingCount: number;
+}
