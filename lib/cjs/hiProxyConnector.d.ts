6eb67ff66183c90cd575cfb94e2668373a63bd54
Andy <poi5305@gmail.com>
2023-03-16 17:02:18 +0800

    build and publish

--- lib/cjs/hiProxyConnector.d.ts
+++ lib/cjs/hiProxyConnector.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="node" />
 import Request from 'request';
 import { ProxyServer } from './struct';
-type RequestOption = Request.CoreOptions & Request.RequiredUriUrl & {
+export type RequestOption = Request.CoreOptions & Request.RequiredUriUrl & {
     lookup?: (domain: any, options: any, callback: any) => void;
 };
 export declare class HiProxyConnector {
@@ -9,15 +9,14 @@ export declare class HiProxyConnector {
     masterPort: number;
     socksPort: number;
     constructor(masterIP: string, masterPort?: number, socksPort?: number);
-    getProxyServers(useCountLessThan: number): Promise<ProxyServer[]>;
-    occupyProxy(allowUsingCount: number, retry?: number): Promise<null | ProxyServer>;
+    getProxyServers(useCountLessThan: number, source?: string): Promise<ProxyServer[]>;
+    occupyProxy(allowUsingCount: number, source?: string, usingSeconds?: number, retry?: number): Promise<null | ProxyServer>;
     releaseProxy(proxyId: string, refresh: boolean): Promise<void>;
     releaseAllProxies(refresh: boolean): Promise<void>;
-    occupyAndRun<T>(allowUsingCount: number, retry: number, release: boolean, task: (proxy: ProxyServer) => Promise<T>): Promise<T | Error>;
-    wrapOptionWithProxyServer(option: RequestOption, proxyServer: ProxyServer, ipFamily: 4 | 6): RequestOption;
-    socksRequestWithOptions(options: RequestOption[], refresh: boolean): Promise<{
+    occupyAndRun<T>(allowUsingCount: number, source: string | undefined, usingSeconds: number | undefined, retry: number | undefined, release: boolean | undefined, task: (proxy: ProxyServer) => Promise<T>): Promise<T | Error>;
+    wrapOptionWithProxyServer(option: RequestOption, proxyServer: ProxyServer, ipFamily: 4 | 6, account?: string, password?: string): RequestOption;
+    socksRequestWithOptions(options: RequestOption[], allowUsingCount: number, source?: string, usingSeconds?: number, retry?: number, release?: boolean): Promise<{
         proxyServer: ProxyServer;
         results: (string | Buffer | Error)[];
     }>;
 }
-export {};
